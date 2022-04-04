2a522db5f8a1d22cfc3ef44f95457544428cf2f3
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2022-04-04 17:07:37 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -22,20 +22,6 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 getData();	
 	
 	
-function send()
-{
-  msg = document.getElementById("msg").value;
-  firebase.database().ref(room_name).push({
-    name:user_name,
-    message:msg,
-    like:0
-   });
 
-  document.getElementById("msg").value = "";
-}
 
-function logout() {
-localStorage.removeItem("user_name");
-localStorage.removeItem("room_name");
-window.location.replace("index.html");
-}
+
