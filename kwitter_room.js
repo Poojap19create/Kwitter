4a1aab2808b703ee72ab05167d9f2fe409e1d185
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2022-04-04 17:13:47 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -48,8 +48,3 @@ function redirectToRoomName(name)
     window.location = "kwitter_page.html";
 }
 
-function logout() {
-localStorage.removeItem("user_name");
-localStorage.removeItem("room_name");
-    window.location = "index.html";
-}
